4fbd1d1f5b230db3b8d330a7559f8d1b44c4b18b
zhongji <zhongj@cdmhzx.com>
2019-08-06 18:02:55 +0800

    升级依赖 删除ant ivy构建文件

--- featherfly-web/src/cn/featherfly/web/servlet/ServletEnv.java
+++ featherfly-web/src/cn/featherfly/web/servlet/ServletEnv.java
@@ -8,9 +8,9 @@ import javax.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import cn.featherfly.common.exception.AssertStandardSys;
+import cn.featherfly.common.lang.Assert;
 import cn.featherfly.common.lang.UriUtils;
-
+import cn.featherfly.web.WebException;
 
 /**
  * <p>
@@ -21,82 +21,89 @@ import cn.featherfly.common.lang.UriUtils;
  */
 public class ServletEnv {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(ServletEnv.class);
+    private static final Assert ASSERT = new Assert(WebException.class);
 
-	private static final String WEBINF_PATH = "WEB-INF";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServletEnv.class);
 
-	private static final String CLASSES_PATH = WEBINF_PATH  + "/classes" ;
+    private static final String WEBINF_PATH = "WEB-INF";
 
-	private static final String LIB_PATH = WEBINF_PATH + "/lib";
+    private static final String CLASSES_PATH = WEBINF_PATH + "/classes";
 
-	private static final ServletEnv env = new ServletEnv();
+    private static final String LIB_PATH = WEBINF_PATH + "/lib";
 
-	/**
-	 * <p>
-	 * 初始化
-	 * </p>
-	 * @param servletContext ServletContext
-	 */
-	public static void init(ServletContext servletContext) {
-		env.projectDir = servletContext.getRealPath("/");
-		LOGGER.debug("项目根目录：{}", env.projectDir);
-		env.webinfDir = new File(UriUtils.linkUri(env.projectDir + WEBINF_PATH)).getAbsolutePath();
-		LOGGER.debug("项目WEB-INF目录：{}", env.webinfDir);
-		env.classesDir = new File(UriUtils.linkUri(env.projectDir + CLASSES_PATH)).getAbsolutePath();
-		LOGGER.debug("项目classes目录：{}", env.classesDir);
-		env.libDir = new File(UriUtils.linkUri(env.projectDir + LIB_PATH)).getAbsolutePath();
-		LOGGER.debug("项目lib目录：{}", env.libDir);
-	}
+    private static final ServletEnv env = new ServletEnv();
 
-	/**
-	 * <p>
-	 * 获取ServletEnv
-	 * </p>
-	 * @return
-	 */
-	public static ServletEnv getEnv() {
-		AssertStandardSys.isNotEmpty(env.projectDir, "ServletEnv还没有初始化");
-		return env;
-	}
+    /**
+     * <p>
+     * 初始化
+     * </p>
+     * 
+     * @param servletContext ServletContext
+     */
+    public static void init(ServletContext servletContext) {
+        env.projectDir = servletContext.getRealPath("/");
+        LOGGER.debug("项目根目录：{}", env.projectDir);
+        env.webinfDir = new File(UriUtils.linkUri(env.projectDir + WEBINF_PATH)).getAbsolutePath();
+        LOGGER.debug("项目WEB-INF目录：{}", env.webinfDir);
+        env.classesDir = new File(UriUtils.linkUri(env.projectDir + CLASSES_PATH)).getAbsolutePath();
+        LOGGER.debug("项目classes目录：{}", env.classesDir);
+        env.libDir = new File(UriUtils.linkUri(env.projectDir + LIB_PATH)).getAbsolutePath();
+        LOGGER.debug("项目lib目录：{}", env.libDir);
+    }
 
-	private String projectDir;
+    /**
+     * <p>
+     * 获取ServletEnv
+     * </p>
+     * 
+     * @return
+     */
+    public static ServletEnv getEnv() {
+        ASSERT.isNotEmpty(env.projectDir, "ServletEnv还没有初始化");
+        return env;
+    }
 
-	private String classesDir;
+    private String projectDir;
 
-	private String webinfDir;
+    private String classesDir;
 
-	private String libDir;
+    private String webinfDir;
 
-	/**
-	 * 返回projectDir
-	 * @return projectDir
-	 */
-	public String getProjectDir() {
-		return projectDir;
-	}
+    private String libDir;
 
-	/**
-	 * 返回classesDir
-	 * @return classesDir
-	 */
-	public String getClassesDir() {
-		return classesDir;
-	}
+    /**
+     * 返回projectDir
+     * 
+     * @return projectDir
+     */
+    public String getProjectDir() {
+        return projectDir;
+    }
 
-	/**
-	 * 返回webinfDir
-	 * @return webinfDir
-	 */
-	public String getWebinfDir() {
-		return webinfDir;
-	}
+    /**
+     * 返回classesDir
+     * 
+     * @return classesDir
+     */
+    public String getClassesDir() {
+        return classesDir;
+    }
 
-	/**
-	 * 返回libDir
-	 * @return libDir
-	 */
-	public String getLibDir() {
-		return libDir;
-	}
+    /**
+     * 返回webinfDir
+     * 
+     * @return webinfDir
+     */
+    public String getWebinfDir() {
+        return webinfDir;
+    }
+
+    /**
+     * 返回libDir
+     * 
+     * @return libDir
+     */
+    public String getLibDir() {
+        return libDir;
+    }
 }
