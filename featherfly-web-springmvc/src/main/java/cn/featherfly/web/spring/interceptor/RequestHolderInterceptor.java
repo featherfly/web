6b0ecccde52c26c94a36effa1f1e00878d191b10
zhongj <featherfly@foxmail.com>
2025-03-28 18:27:44 +0800

    fix: RequestHolderInterceptor clear ThreadLocal resource

--- featherfly-web-springmvc/src/main/java/cn/featherfly/web/spring/interceptor/RequestHolderInterceptor.java
+++ featherfly-web-springmvc/src/main/java/cn/featherfly/web/spring/interceptor/RequestHolderInterceptor.java
@@ -40,7 +40,7 @@ public class RequestHolderInterceptor implements AsyncHandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
         throws Exception {
-
+        holder.remove();
     }
 
     /**
